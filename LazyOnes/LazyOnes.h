f4d2f00fad22f3de0a0c2c15cb67b828a856bb56
ryan <ryan@ryandemacbook-pro.local>
2018-05-15 15:27:19 +0800

    Initial Commit

---
+++ LazyOnes/LazyOnes.h
@@ -0,0 +1,19 @@
+//
+//  LazyOnes.h
+//  LazyOnes
+//
+//  Created by ryan on 2018/5/15.
+//  Copyright © 2018年 Lifebank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LazyOnes.
+FOUNDATION_EXPORT double LazyOnesVersionNumber;
+
+//! Project version string for LazyOnes.
+FOUNDATION_EXPORT const unsigned char LazyOnesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LazyOnes/PublicHeader.h>
+
+
