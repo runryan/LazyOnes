0be2bca9f58fee12e8ff7674735c9bff7ca8655f
runryan <mob_developer@163.com>
2018-05-15 15:43:01 +0800

    创建Lazy类

---
+++ LazyOnes/Lazy.swift
@@ -0,0 +1,16 @@
+//
+//  Lazy.swift
+//  LazyOnes
+//
+//  Created by ryan on 2018/5/15.
+//  Copyright © 2018年 Lifebank. All rights reserved.
+//
+
+import UIKit
+
+public class Lazy: NSObject {
+
+    func haha() {
+        print("Hahahahahahahaha!")
+    }
+}
